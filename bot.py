b4f28b81766e5d6196476e06a237f6b528954318
vnadvornyak <vnadvornyak@mail.ru>
2022-11-12 18:40:40 +0600

    123

---
+++ bot.py
@@ -0,0 +1,3 @@
+from
+
+bot_token = '5689722623:AAHMZ_X-BTr2dAJNaotC_dCKRhitzUIcnMQ'
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import requests
 
-name = input()
+name = 'Австралия'
 response = requests.get(
     f'http://api.openweathermap.org/data/2.5/weather?q={name}&appid=4321a3d417b53045aa1b6617c529c910&units=metric&lang=ru')
 a = response.json()
 print(a)
+print(a['weather'][0]['description'])  # описание
+print(a['main']['temp']) # температура
+print(a['main']['feels_like']) # ощущается как
+print(a['wind']['speed']) # ощущается как
+
