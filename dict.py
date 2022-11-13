d0bbb277dbb44f711f5c65bef1572461d15a1c5b
vnadvornyak <vnadvornyak@mail.ru>
2022-11-13 12:30:29 +0600

    dictionary

--- dict.py
+++ dict.py
@@ -1,5 +1,15 @@
 import openpyxl
 
+
+def create_list():
+    book = openpyxl.open('country_dict.xlsx')
+    sheet = book.active
+    country_list = []
+    for i in range(1, sheet.max_row+1):
+        country_list.append(sheet[i][0].value)
+    return country_list
+
+
 def create_dict():
     book = openpyxl.open('country_dict.xlsx')
     sheet = book.active
@@ -9,8 +19,6 @@ def create_dict():
     return country_dict
 
 
-def find_county_name(name: str, d: dict):
+def find_country_name(name: str, d: dict):
     eng_name = d[name]
     return eng_name
-
-
